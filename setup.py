5aae1459955424ae1e68f764885363049b7db5f7
Tsundukani <tsundukani.makhubela@gmail.com>
2020-02-19 16:26:40 +0200

    Renamed the package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='analyse-sprint-package',
+    name='analyse_sprint_package',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
